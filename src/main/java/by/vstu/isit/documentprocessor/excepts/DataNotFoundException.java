21ed3e9572cb69c8379bd73eb0775d0bf64f054f
PawelGregDerk <Derkachenko203509@gmail.com>
2026-01-04 19:00:04 +0300

    API refactoring

--- src/main/java/by/vstu/isit/documentprocessor/excepts/DataNotFoundException.java
+++ src/main/java/by/vstu/isit/documentprocessor/excepts/DataNotFoundException.java
@@ -1,11 +1,14 @@
 package by.vstu.isit.documentprocessor.excepts;
 
+import static by.vstu.isit.documentprocessor.utils.MessageCodes.DATA_NOT_FOUND;
+import by.vstu.isit.documentprocessor.utils.LocalizeHelper;
+
 public class DataNotFoundException extends RuntimeException {
     public DataNotFoundException(String msg) {
         super(msg);
     }
 
     public DataNotFoundException() {
-        this("data.not.found");
+        this(LocalizeHelper.getMessage(DATA_NOT_FOUND));
     }
 }
