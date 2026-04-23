1736df3ae91c878c81adda03043a416cd75cd0ae
PawelGregDerk <Derkachenko203509@gmail.com>
2026-04-23 21:39:09 +0300

    with full editing

--- src/main/java/by/vstu/isit/documentprocessor/services/docx/edit/PuDocEditor.java
+++ src/main/java/by/vstu/isit/documentprocessor/services/docx/edit/PuDocEditor.java
@@ -3,15 +3,15 @@ package by.vstu.isit.documentprocessor.services.docx.edit;
 import by.vstu.isit.documentprocessor.dto.DockPackageDto;
 import by.vstu.isit.documentprocessor.dto.FuncDto;
 import by.vstu.isit.documentprocessor.dto.OperDto;
+import com.spire.doc.BookmarkStart;
 import com.spire.doc.Table;
 import com.spire.doc.*;
+import com.spire.doc.documents.Paragraph;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 @Service
 public class PuDocEditor extends AbstractDocEditor {
@@ -30,48 +30,13 @@ public class PuDocEditor extends AbstractDocEditor {
         Document doc = loadCopyByBookmark(dto.path(), dto.puName(), savedDto.puName(), 0, sourceBookmark(dto));
         Table table = doc.getSections().get(0).getTables().get(0);
 
-        Set<Long> processedFuncIds = new HashSet<>();
-
-        for (int oi = 0; oi < savedDto.opers().size(); oi++) {
-            OperDto oper = savedDto.opers().get(oi);
-            OperDto origOper = oi < dto.opers().size() ? dto.opers().get(oi) : null;
+        for (OperDto oper : savedDto.opers()) {
             updateCell(table, "oper_" + oper.id() + "_col_0", oper.numOper());
             updateCell(table, "oper_" + oper.id() + "_col_1", oper.name());
             updateCell(table, "oper_" + oper.id() + "_oborud", oper.oborud());
             updateCell(table, "oper_" + oper.id() + "_ostnasInstr", oper.ostnasInstr());
-
-            for (FuncDto func : oper.funcs()) {
-                processedFuncIds.add(func.id());
-                boolean exists = findCellByBookmark(table, "func_" + func.id() + "_col_6") != null;
-
-                if (exists) {
-                    boolean originalIsProd = func.isProd();
-                    if (origOper != null) {
-                        originalIsProd = origOper.funcs().stream()
-                                .filter(f -> f.id() != null && f.id().equals(func.id()))
-                                .map(FuncDto::isProd)
-                                .findFirst().orElse(func.isProd());
-                    }
-                    if (originalIsProd != func.isProd()) {
-                        clearBookmarkText(table, "func_" + func.id() + "_col_4");
-                        clearBookmarkText(table, "func_" + func.id() + "_col_5");
-                    }
-                    updateCell(table, "func_" + func.id() + "_col_4", func.isProd() ? func.name() : "");
-                    updateCell(table, "func_" + func.id() + "_col_5", func.isProd() ? "" : func.name());
-                    updateCell(table, "func_" + func.id() + "_col_6", func.specCharakt());
-                    updateCell(table, "func_" + func.id() + "_col_7", func.param());
-                } else {
-                    TableRow newRow = insertRowAfterLastBookmark(table, "oper_" + oper.id(), COLUMN_COUNT);
-                    setShadedBookmarkedText(newRow.getCells().get(func.isProd() ? 4 : 5),
-                            "func_" + func.id() + "_col_" + (func.isProd() ? "4" : "5"),
-                            func.name());
-                    setShadedBookmarkedText(newRow.getCells().get(6), "func_" + func.id() + "_col_6", func.specCharakt());
-                    setShadedBookmarkedText(newRow.getCells().get(7), "func_" + func.id() + "_col_7", func.param());
-                }
-            }
+            rebuildOperationFunctionRows(table, oper);
         }
-
-        removeDeletedRows(table, processedFuncIds, "func_", "_col_6");
         rebuildOperMerges(table, savedDto);
 
         String article = savedDto.sborEds().getFirst().nazv() + " " + designationsAssemblyUnit(savedDto.sborEds());
@@ -120,6 +85,93 @@ public class PuDocEditor extends AbstractDocEditor {
             table.applyVerticalMerge(2, start, end);
             table.applyVerticalMerge(12, start, end);
             table.applyVerticalMerge(13, start, end);
+        }
+    }
+
+    private void rebuildOperationFunctionRows(Table table, OperDto oper) {
+        TableRow startRow = findRowByBookmark(table, "oper_" + oper.id() + "_col_0");
+        if (startRow == null) {
+            return;
+        }
+        int start = rowIndexOf(table, startRow);
+        if (start < 0) {
+            return;
+        }
+
+        int blockEnd = findOperationBlockEnd(table, start);
+        int targetRows = Math.max(oper.funcs().size(), 1);
+        int currentRows = blockEnd - start + 1;
+
+        while (currentRows < targetRows) {
+            TableRow template = table.getRows().get(blockEnd);
+            TableRow newRow = (TableRow) template.deepClone();
+            clearRowTexts(newRow);
+            table.getRows().insert(blockEnd + 1, newRow);
+            blockEnd++;
+            currentRows++;
+        }
+
+        while (currentRows > targetRows) {
+            table.getRows().remove(table.getRows().get(blockEnd));
+            blockEnd--;
+            currentRows--;
+        }
+
+        for (int i = 0; i < oper.funcs().size(); i++) {
+            FuncDto func = oper.funcs().get(i);
+            TableRow row = table.getRows().get(start + i);
+            setShadedBookmarkedText(row.getCells().get(4), "func_" + func.id() + "_col_4",
+                    Boolean.TRUE.equals(func.isProd()) ? func.name() : "");
+            setShadedBookmarkedText(row.getCells().get(5), "func_" + func.id() + "_col_5",
+                    Boolean.TRUE.equals(func.isProd()) ? "" : func.name());
+            setShadedBookmarkedText(row.getCells().get(6), "func_" + func.id() + "_col_6", func.specCharakt());
+            setShadedBookmarkedText(row.getCells().get(7), "func_" + func.id() + "_col_7", func.param());
+        }
+    }
+
+    private int rowIndexOf(Table table, TableRow row) {
+        for (int i = 0; i < table.getRows().getCount(); i++) {
+            if (table.getRows().get(i) == row) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private int findOperationBlockEnd(Table table, int startIndex) {
+        for (int i = startIndex + 1; i < table.getRows().getCount(); i++) {
+            if (containsOperStartBookmark(table.getRows().get(i))) {
+                return i - 1;
+            }
+        }
+        return table.getRows().getCount() - 1;
+    }
+
+    private boolean containsOperStartBookmark(TableRow row) {
+        for (int j = 0; j < row.getCells().getCount(); j++) {
+            TableCell cell = row.getCells().get(j);
+            for (int k = 0; k < cell.getParagraphs().getCount(); k++) {
+                Paragraph para = cell.getParagraphs().get(k);
+                for (int m = 0; m < para.getChildObjects().getCount(); m++) {
+                    Object obj = para.getChildObjects().get(m);
+                    if (obj instanceof BookmarkStart bm) {
+                        String name = bm.getName();
+                        if (name != null && name.startsWith("oper_") && name.endsWith("_col_0")) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private void clearRowTexts(TableRow row) {
+        for (int j = 0; j < row.getCells().getCount(); j++) {
+            TableCell cell = row.getCells().get(j);
+            for (int k = 0; k < cell.getParagraphs().getCount(); k++) {
+                cell.getParagraphs().get(k).setText("");
+            }
         }
     }
 }
